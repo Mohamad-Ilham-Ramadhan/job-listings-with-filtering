7c5b0ad468b555267467420092dc55172b34a3bf
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2020-08-18 13:11:16 +0700

    change component's file extension to .jsx | AirBnb guide [make it right]

--- src/components/FilterSelect.jsx
+++ src/components/FilterSelect.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { makestyles, makeStyles } from "@material-ui/core/styles";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
 import Paper from "@material-ui/core/Paper";
 import Popover from "@material-ui/core/Popover";
--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import { render } from "react-dom";
 import App from "./components/App";
 
 render(<App />, document.getElementById("root"));
+
+// 1. AirBnb React/JSX style guide
